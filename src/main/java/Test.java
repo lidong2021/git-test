559003d6ea2164fe1c475f4de1e95223afe6ceda
lidong <lidong@ztgeo.com.cn>
2021-08-06 15:12:25 +0800

    hot-fix commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("111111111");
         System.out.println("2222222222");
         System.out.println("333333333333");
+        System.out.println("hot-fix");
     }
 }
